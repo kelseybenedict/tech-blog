11095ee73fb69e78401e87aab4116452c6cf8de4
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-04-11 23:31:20 -0600

    modified css and handblebars

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,10 +2,9 @@ const newFormHandler = async (event) => {
   event.preventDefault();
 
   const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
   const description = document.querySelector('#project-desc').value.trim();
 
-  if (name && needed_funding && description) {
+  if (name && description) {
     const response = await fetch(`/api/projects`, {
       method: 'POST',
       body: JSON.stringify({ name, needed_funding, description }),
@@ -17,7 +16,7 @@ const newFormHandler = async (event) => {
     if (response.ok) {
       document.location.replace('/profile');
     } else {
-      alert('Failed to create project');
+      alert('Failed to create post');
     }
   }
 };
