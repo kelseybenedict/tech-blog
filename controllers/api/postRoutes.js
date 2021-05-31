5ac01317799d9fe2a6d626a7eab103367561983b
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-05-30 20:45:28 -0600

    fixed post routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -16,19 +16,39 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    // delete post with specific id
+    const postData = await Post.destroy({
       where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
+        id: req.params.id
       },
     });
-
-    if (!projectData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+    // error handling if user enters an invalid post id to delete
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
+    res.status(200).json(postData);
+    // error handling
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-    res.status(200).json(projectData);
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    // updating the post with specific id
+    const [affectedRows] = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    // if updated text is greater than 0
+    if (affectedRows > 0) {
+      res.status(200).end();
+      // otherwise if it's blank, not a valid update
+    } else {
+      res.status(404).end();
+    }
   } catch (err) {
     res.status(500).json(err);
   }
