80b00690e99386b99ad4eb69b955e0cde833cbba
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-05-30 22:02:52 -0600

    fixed home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -52,11 +52,20 @@ router.get('/post/:id', async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/');
     return;
   }
 
   res.render('login');
+});
+// signup route
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
 });
 
 module.exports = router;
