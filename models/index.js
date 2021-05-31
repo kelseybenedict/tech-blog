819e05dd093ab09981dd550988fa1764f10c4650
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-05-30 19:44:33 -0600

    modified index model

--- models/index.js
+++ models/index.js
@@ -1,13 +1,27 @@
 const User = require('./User');
-const Project = require('./Project');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
+// belongs to a single user
+Post.belongsTo(User, {
+  foreignKey: 'userId',
   onDelete: 'CASCADE'
 });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
+//belongs to a single user
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
 });
 
-module.exports = { User, Project };
+// a post can have multiple comments
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
+
+module.exports = {
+  User,
+  Comment,
+  Post
+};
